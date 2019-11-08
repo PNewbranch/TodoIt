5d07ea333d2671a8d7307389ec7003b1433eaa19
P N <p.newbranch@outlook.com>
2019-11-08 15:59:14 +0100

    Working on step 6.

---
+++ TodoIt/Data/PersonSequencer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Data
+{
+    class PersonSequencer
+    {
+        private static int personId;
+
+        static int nextPersonId()
+        {
+            return personId--;
+        }
+
+        static void reset ()
+        {
+            personId = 0;
+        }
+
+    }
+}
---
+++ XUnitTestProjectTodoIt/TestPersonSeguencer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Text;
+using TodoIt.Model;
+using Xunit;
+
+namespace XUnitTestProjectTodoIt
+{
+    public class TestPersonSeguencer
+    {
+
+
+        [Fact]
+        int personId = 0;
+
+        public int increasePersonSequencer (int personId)
+        {
+            return personId--;
+
+
+        //[Fa]
+        //public void 
+        ////Arrange
+        //private static int personId = 0;
+
+        ////Act
+        //static pers nextPersonalId()
+        //{
+        //    return nextPersonalId;
+        //}
+
+        ////Assert
+        }
+
+
+    }
+}
